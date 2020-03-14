226e1d155a443db9334905870b5a8a24d446e7e5
MapleRen <kl941104@163.com>
2020-03-14 18:07:09 +0800

    Update Unblock.js

--- QuantumultX/Unblock.js
+++ QuantumultX/Unblock.js
@@ -7,7 +7,7 @@ var body = $response.body;
 var obj = JSON.parse(body);
 
 obj['content']['notices'] = [];
-obj['content']['isWhitelist'] = true;
+obj['content']['isWhitelist'] = false;
 body = JSON.stringify(obj);
 
 console.log(body);
